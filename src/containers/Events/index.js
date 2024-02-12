87d254e9797f7e25a3f43ab9728bde1431a72230
morganhel <138800018+morganhel@users.noreply.github.com>
2024-02-12 15:01:21 +0100

    Debug filtrer réalisation

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,19 +13,18 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
+
+  const filteredEvents = (data?.events || [])
+  .filter((event) => {
+    if (!type || event.type === type) {
       return true;
     }
     return false;
-  });
+  })
+  .slice((currentPage - 1) * PER_PAGE, currentPage * PER_PAGE);
+
+  console.log(filteredEvents);
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
